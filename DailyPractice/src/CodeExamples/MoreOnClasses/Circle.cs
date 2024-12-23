c6eca496cbe962c1b86da2d561f4950d81f94e1d
Iqbal Hassan Mahmud <iqbalmahmud.cs@gmail.com>
2024-12-23 17:25:39 +0600

    04. Working More with Classes

---
+++ DailyPractice/src/CodeExamples/MoreOnClasses/Circle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreOnClasses
+{
+    public class Circle : Shape
+    {
+        public static int Count { get; private set; }
+
+        public Circle()
+        {
+            Count++;
+        }
+        public double Radius {  get; set; }
+        public override double CalculateArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+    }
+}
---
+++ DailyPractice/src/CodeExamples/MoreOnClasses/RestictedDiscount.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreOnClasses
+{
+    public class RestictedDiscount : Discount
+    {
+        public sealed override double GetAmount(double amount)
+        {
+            double discountedAmount = DiscountAmount * amount / 100;
+            if (discountedAmount > amount / 2)
+            {
+                return amount / 2;
+            }
+            else
+                return discountedAmount;
+        }
+    }
+}
