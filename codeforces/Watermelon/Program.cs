562a5972fca3dfa3c150548fad047a9df01c9afd
Iqbal Hassan Mahmud <iqbalmahmud.cs@gmail.com>
2024-12-24 01:56:28 +0600

    71A - Way Too Long Words

--- codeforces/Watermelon/Program.cs
+++ codeforces/Watermelon/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Codeforces!");
+﻿int w = int.Parse(Console.ReadLine());
+
+if (w > 2 && w % 2 == 0)
+{
+    Console.WriteLine("YES");
+}
+else
+{
+    Console.WriteLine("NO");
+}
