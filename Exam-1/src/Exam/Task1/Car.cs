52326c89f1b44d57b50cfbcb4e01d616745efeb2
Iqbal Hassan Mahmud <iqbalmahmud.cs@gmail.com>
2024-12-27 00:12:50 +0600

    task-1

---
+++ Exam-1/src/Exam/Task1/Car.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Car : Machine
+    {
+        public override void Start()
+        {
+            Console.WriteLine("Car Started");
+        }
+    }
+}
