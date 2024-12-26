4d6bfaa31e3e83f5ecdaa36e140e58d6fd2e0e82
Iqbal Hassan Mahmud <iqbalmahmud.cs@gmail.com>
2024-12-26 23:59:27 +0600

    zz

---
+++ Exam-1/src/Exam/Task2/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    public abstract class Person
+    {
+        internal string FirstName { get; set; }
+        internal string LasttName { get; set; }
+        internal DateTime DateOfBirth { get; set; }
+
+        public abstract void DisplayDetails();
+
+
+    }
+}
