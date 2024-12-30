9505736409dddc5ae7b4f793d1361b023902bb8b
Iqbal Hassan Mahmud <iqbalmahmud.cs@gmail.com>
2024-12-30 18:14:54 +0600

    Added HelpfulMaths.csproj

---
+++ Assignment-1/src/HelpfulMaths/Program.cs
@@ -0,0 +1,15 @@
+﻿string s = Console.ReadLine();
+
+if (s.Length > 100)
+    return;
+
+if (!s.All(c => c == '+' || c == '1' || c == '2' || c == '3'))
+    return;
+
+string[] numbers = s.Split('+');
+int[] shortedNumber = Array.ConvertAll(numbers, int.Parse);
+Array.Sort(shortedNumber);
+
+string result = string.Join("+", shortedNumber);
+
+Console.WriteLine(result);
