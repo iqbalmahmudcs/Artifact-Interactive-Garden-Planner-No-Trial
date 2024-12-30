79adcd21d3d2afbd7e6fe0933ddf1d9636aaf76a
Iqbal Hassan Mahmud <iqbalmahmud.cs@gmail.com>
2024-12-30 16:45:48 +0600

    Added git ignore file and Rename the folder codeforces to Assignment-1

---
+++ Assignment-1/NextRound/Program.cs
@@ -0,0 +1,21 @@
+﻿string[] input = Console.ReadLine().Split();
+int n = int.Parse(input[0]);
+int k = int.Parse(input[1]);
+
+if(k > 0 && n >= k && k <= 50 && n <= 50)
+{
+    int nextRound = 0;
+    int[] scores = new int[n];
+    input =  Console.ReadLine().Split();
+    for(int i = 0; i < input.Length; i++)
+    {
+        scores[i]= int.Parse(input[i]);
+    }
+    int kthScore = scores[k-1];
+    for(int i = 0;i < scores.Length; i++)
+    {
+        if(scores[i] >= kthScore && scores[i] > 0)
+            nextRound++;
+    }
+    Console.WriteLine(nextRound);
+}
---
+++ Assignment-1/Team/Program.cs
@@ -0,0 +1,26 @@
+﻿int n = int.Parse(Console.ReadLine());
+int count = 0;
+if(n > 0 && n <= 1000)
+{
+    for (int i = 0; i < n; i++)
+    {
+        string[] input = Console.ReadLine().Split();
+        int petya = int.Parse(input[0]);
+        int vasy = int.Parse(input[1]);
+        int tonya = int.Parse(input[2]);
+
+        if (petya < 0 || petya > 1 || vasy < 0 || vasy > 1 || tonya < 0 || tonya > 1)
+        {
+        i--;
+        continue;
+        }
+        else
+        {
+            if (petya + vasy + tonya >= 2)
+            {
+                count++;
+            }
+        }
+    }
+    Console.WriteLine(count);
+}
