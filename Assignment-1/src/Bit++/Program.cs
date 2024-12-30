c3ca58da0d064f04845d58ae7020b13c472b0ab5
Iqbal Hassan Mahmud <iqbalmahmud.cs@gmail.com>
2024-12-30 17:23:07 +0600

    Created src folder adn all the source codes are moved there.

---
+++ Assignment-1/src/Bit++/Program.cs
@@ -0,0 +1,18 @@
+﻿int n = int.Parse(Console.ReadLine());
+if (n > 0 && n <= 150)
+{
+    int x = 0;
+    for (int i = 0; i < n; i++)
+    {
+        string statement = Console.ReadLine();
+        if (statement.Contains("X++") || statement.Contains("x++"))
+            x++;
+        else if (statement.Contains("++X") || statement.Contains("++x"))
+            ++x;
+        else if (statement.Contains("X--") || statement.Contains("x--"))
+            x--;
+        else
+            --x;
+    }
+    Console.WriteLine(x);
+}
