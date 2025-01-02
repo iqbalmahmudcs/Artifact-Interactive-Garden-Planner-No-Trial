59a4e31eec12a87f7b70ced99b0ce0b0727bab12
Iqbal Hassan Mahmud <iqbalmahmud.cs@gmail.com>
2025-01-02 15:18:04 +0600

    MethodOverloadingExample

---
+++ DailyPractice/src/CodeExamples/MethodOverLoadingExample/Calculator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverLoadingExample
+{
+    public class Calculator
+    {
+        public int Calculation(int a, int b) { return a + b; }
+        
+        //method overloading (Number of parameters)
+        public int Calculation(int a, int b, int c) { return a + b + c; }
+        //methid overloading different types of parameter
+        public double Calculation(double a, double b) { return a + b; }
+    }
+}
---
+++ DailyPractice/src/CodeExamples/MethodOverLoadingExample/Program.cs
@@ -0,0 +1,8 @@
+﻿using MethodOverLoadingExample;
+
+Calculator calculator = new Calculator();
+
+
+Console.WriteLine(calculator.Calculation(1, 2));
+Console.WriteLine(calculator.Calculation(1, 2, 3));
+Console.WriteLine($"{calculator.Calculation(1.1, 2.2):F1}");
