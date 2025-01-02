282b59f920af9a7e169df4f08f17cd5b9612c71a
Iqbal Hassan Mahmud <iqbalmahmud.cs@gmail.com>
2025-01-02 16:54:24 +0600

    AreaCalculatorWithOverloadedMethods

---
+++ DailyPractice/src/CodeExamples/AreaCalculatorWithOverloadedMethods/AreaCalculator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaCalculatorWithOverloadedMethods
+{
+    public class AreaCalculator
+    {
+        public double CalculateArea(double radius) { return Math.PI * radius * radius; }
+        public int CalculateArea(int  lenth, int width) { return lenth * width; }
+        public double CalculateArea(double  baze, double height) { return 0.5 * baze * height; }
+    }
+}
--- DailyPractice/src/CodeExamples/AreaCalculatorWithOverloadedMethods/Program.cs
+++ DailyPractice/src/CodeExamples/AreaCalculatorWithOverloadedMethods/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using AreaCalculatorWithOverloadedMethods;
+
+AreaCalculator areaCalculator = new AreaCalculator();
+
+Console.WriteLine($"Area of Circle: {areaCalculator.CalculateArea(1.1):F2}");
+Console.WriteLine($"Area of Rectangle: {areaCalculator.CalculateArea(1,2)}");
+Console.WriteLine($"Area of Triangle: {areaCalculator.CalculateArea(1,2):F2}");
