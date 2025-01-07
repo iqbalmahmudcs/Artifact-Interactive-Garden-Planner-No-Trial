115dbab55bf7a3ac3be639dc1660cc5ac009b29a
Iqbal Hassan Mahmud <iqbalmahmud.cs@gmail.com>
2025-01-07 18:16:27 +0600

    11WordCapitalization

---
+++ Assignment-1/src/11WordCapitalization/Program.cs
@@ -0,0 +1,8 @@
+﻿string input = Console.ReadLine();
+
+if (string.IsNullOrEmpty(input) || input.Length > 1000)
+    return;
+
+string output = char.ToUpper(input[0]) + input.Substring(1);
+
+Console.WriteLine(output);
