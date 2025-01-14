31eb87a1af629c09dcf378cdfcea789972b3ff4f
Iqbal Hassan Mahmud <iqbalmahmud.cs@gmail.com>
2025-01-14 19:15:51 +0600

    10. Working with Entity Framework

---
+++ DailyPractice/src/CodeExamples/ADONetExample/DataUtility.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADONetExample
+{
+    public   class DataUtility
+    {
+        private readonly string _connectionString;
+        public DataUtility(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+        public void ExecuteSql(string sql, Dictionary<string, object> parameters)
+        {
+            using var sqlCommand = GetCommand(sql, parameters);
+
+            int effected = sqlCommand.ExecuteNonQuery();
+
+        }
+        public List<Dictionary<string, object>> GetData(string sql, Dictionary<string, object> parameters)
+        {
+            using var sqlCommand = GetCommand(sql, parameters);
+            using var reader = sqlCommand.ExecuteReader();
+            
+            var data = new List<Dictionary<string, object>>();
+            
+            while (reader.Read()) 
+            {
+                var row = new Dictionary<string, object>();
+                for (int i = 0; i < reader.FieldCount; i++) 
+                {
+                    row.Add(reader.GetName(i), reader[i]);
+                }
+                data.Add(row);
+            }
+
+            return data;
+        }
+
+        private SqlCommand GetCommand(string sql, Dictionary<string, object> parameters) 
+        {
+            var sqlConnection = new SqlConnection(_connectionString);
+            var sqlCommand = new SqlCommand(sql, sqlConnection);
+
+            if (sqlConnection.State != System.Data.ConnectionState.Open)
+            {
+                sqlConnection.Open();
+            }
+
+            foreach (var parameter in parameters)
+            {
+                sqlCommand.Parameters.Add(new SqlParameter(parameter.Key, parameter.Value));
+            }
+
+            return sqlCommand;
+        } 
+
+    }
+}
---
+++ DailyPractice/src/CodeExamples/ADONetExample/Program.cs
@@ -0,0 +1,35 @@
+﻿using ADONetExample;
+var connectionString = "Server=OFFICER-IT-DHAK\\SQLEXPRESS;Database=CSharpB19;User Id=csharpb19;Password=asdf1234Z@; Trust Server Certificate=True";
+DataUtility dataUtility = new DataUtility(connectionString);
+
+//Insert Data
+var name = Console.ReadLine();
+var fees = Console.ReadLine();
+var date = Console.ReadLine();
+var activeStatus = Console.ReadLine();
+
+var sql = $"insert into Courses(Name, Fees, ClassStartDate, IsActive) values(@name, @fees, @date, @activeStatus)";
+//var sql = "delete from Courses where ID = 100";
+var parameters = new Dictionary<string, object>
+{
+    { "name", name},
+    { "fees", fees},
+    { "date", date},
+    { "activeStatus", activeStatus}
+};
+dataUtility.ExecuteSql(sql, parameters);
+
+
+////Read Data
+//var sql1 = "select * from courses where fees >= 15000";
+//var data = dataUtility.GetData(sql1);
+
+//foreach(var item in data)
+//{
+//    foreach(var col in item)
+//    {
+//        Console.Write($"{col.Key} {col.Value}, ");
+//    }
+//    Console.WriteLine();
+//}
+
