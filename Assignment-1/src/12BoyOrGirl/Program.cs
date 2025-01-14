1342b61c07f1bf4a41adebd5707be04515c751e6
Iqbal Hassan Mahmud <iqbalmahmud.cs@gmail.com>
2025-01-14 11:53:06 +0600

    12 & 13 no problem solved.

---
+++ Assignment-1/src/12BoyOrGirl/Program.cs
@@ -0,0 +1,12 @@
+﻿string input = Console.ReadLine().ToLower();
+
+if (string.IsNullOrEmpty(input) | input.Length > 100)
+{
+    return;
+}
+if(input.Distinct().Count() % 2 == 0)
+{
+    Console.WriteLine("CHAT WITH HER!");
+}
+else
+    Console.WriteLine("IGNORE HIM!");
---
+++ Assignment-1/src/13BearAndBigBrother/Program.cs
@@ -0,0 +1,19 @@
+﻿string[] input = Console.ReadLine().Split();
+int a = int.Parse(input[0]); 
+int b = int.Parse(input[1]);
+
+if (a < 1 || b < 1 || b < a || a > 10 || b > 10)
+    return;
+
+int year = 0;
+
+while (a <= b)
+{
+    a *= 3;
+    b *= 2;
+    year++;
+}
+
+Console.WriteLine(year);
+
+
