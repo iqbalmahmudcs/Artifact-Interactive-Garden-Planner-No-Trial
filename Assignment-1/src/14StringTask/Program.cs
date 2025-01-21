993ceaaa3f26b2d38600162e241b1ee418fd9e7e
Iqbal Hassan Mahmud <iqbalmahmud.cs@gmail.com>
2025-01-21 20:13:12 +0600

    14. String Task

---
+++ Assignment-1/src/14StringTask/Program.cs
@@ -0,0 +1,14 @@
+﻿string input = Console.ReadLine();
+
+string vowels = "aoyeuiAOYEUI";
+string result = "";
+
+foreach (char ch in input)
+{
+    if (!vowels.Contains(ch))
+    {
+        result += "." + char.ToLower(ch);
+    }
+}
+
+Console.WriteLine(result);
