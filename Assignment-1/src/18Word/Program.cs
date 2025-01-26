8ce052a5b1a706b1438d02e05dfe0c0d84df54ff
Iqbal Hassan Mahmud <iqbalmahmud.cs@gmail.com>
2025-01-26 17:18:58 +0600

    18Word

---
+++ Assignment-1/src/18Word/Program.cs
@@ -0,0 +1,16 @@
+﻿string s = Console.ReadLine();
+
+if (s.Length < 1 || s.Length > 100)
+    return;
+
+int upperCaseCount = s.Count(char.IsUpper);
+int lowerCaseCount = s.Count(char.IsLower);
+
+string output = null;
+
+if (upperCaseCount > lowerCaseCount)
+    output = s.ToUpper();
+else 
+    output = s.ToLower();
+
+Console.WriteLine(output);
