197c4c439446ba8e13bfebfc9bc50dddc1ad744b
Iqbal Hassan Mahmud <iqbalmahmud.cs@gmail.com>
2025-01-26 10:56:16 +0600

    17SoldierAndBananas

---
+++ Assignment-1/src/17SoldierAndBananas/Program.cs
@@ -0,0 +1,14 @@
+﻿string[] input = Console.ReadLine().Split();
+
+int k = int.Parse(input[0]);
+int n = int.Parse(input[1]);
+int w = int.Parse(input[2]);
+
+int totalCost =  k * (w * (w + 1) / 2);
+
+int moneyNeeded = totalCost - n;
+
+if(moneyNeeded < 0)
+    moneyNeeded = 0;
+
+Console.WriteLine(moneyNeeded);
