0df63da4548031a7ad898eeccb8a883a72202dc5
Iqbal Hassan Mahmud <iqbalmahmud.cs@gmail.com>
2025-01-26 10:44:03 +0600

    15StonesOnTheTable 16Elephant

---
+++ Assignment-1/src/15StonesOnTheTable/Program.cs
@@ -0,0 +1,20 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+if (n < 1 && n > 50)
+    return;
+
+string input = Console.ReadLine();
+
+if (input == null) return;
+if (input.Length > n) return;
+
+int count = 0;     // count of the number of stones to be removed.
+
+for (int i = 1; i < n; i++)
+{
+    if (input[i] == input[i - 1])
+    {
+        count++;
+    }
+}
+Console.WriteLine(count);
---
+++ Assignment-1/src/16Elephant/Program.cs
@@ -0,0 +1,12 @@
+﻿int input = int.Parse(Console.ReadLine());
+
+if (input < 1 || input > 1000000)
+    return;
+
+int stepsCount = input / 5;
+
+if (input % 5 != 0)
+    stepsCount += 1;
+
+
+Console.WriteLine(stepsCount);
