31e345c73d2dddf390f3ab5c331d2e4342c00487
Iqbal Hassan Mahmud <iqbalmahmud.cs@gmail.com>
2025-01-28 20:02:35 +0600

    20Young Physicist

--- Assignment-1/src/20Young Physicist/Program.cs
+++ Assignment-1/src/20Young Physicist/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int n = int.Parse(Console.ReadLine());
+
+int sumX = 0, sumY = 0, sumZ = 0;
+
+for(int i = 0; i < n; i++)
+{
+    string[] input = Console.ReadLine().Split();
+
+    int x = int.Parse(input[0]);
+    int y = int.Parse(input[1]);
+    int z = int.Parse(input[2]);
+
+    sumX += x;
+    sumY += y;
+    sumZ += z;
+}
+
+//if(sumX == 0 && sumY ==0 && sumZ == 0)
+//    Console.WriteLine("YES");
+//else
+//    Console.WriteLine("NO");
+
+Console.WriteLine((sumX == 0 && sumY == 0 && sumZ == 0) ? "YES" : "NO");
