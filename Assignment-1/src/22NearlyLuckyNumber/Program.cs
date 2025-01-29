7f287b986e8b8f187155729d5e3f00e2d985f3a9
Iqbal Hassan Mahmud <iqbalmahmud.cs@gmail.com>
2025-01-29 17:44:42 +0600

    22NearlyLuckyNumber

---
+++ Assignment-1/src/22NearlyLuckyNumber/Program.cs
@@ -0,0 +1,23 @@
+﻿string n = Console.ReadLine();
+
+int luckyCount = 0;
+
+foreach (char c in n)
+{
+    if (c == '4' || c == '7')
+        luckyCount++;
+}
+
+string luckyCountStr = luckyCount.ToString();
+bool isNearlyLucky = true;
+
+foreach (char c in luckyCountStr)
+{
+    if (c != '4' && c != '7')
+    {
+        isNearlyLucky = false;
+        break;
+    }
+}
+
+Console.WriteLine(isNearlyLucky ? "YES" : "NO");
