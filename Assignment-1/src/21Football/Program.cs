c65ff0eb89bcc78481f066183989612da85727b6
Iqbal Hassan Mahmud <iqbalmahmud.cs@gmail.com>
2025-01-29 12:12:41 +0600

    21Football

---
+++ Assignment-1/src/21Football/Program.cs
@@ -0,0 +1,9 @@
+﻿string players = Console.ReadLine();
+
+if (string.IsNullOrEmpty(players) || players.Length > 100)
+    return;
+
+if (players.Contains("0000000") || players.Contains("1111111"))
+    Console.WriteLine("YES");
+else
+    Console.WriteLine("NO");
