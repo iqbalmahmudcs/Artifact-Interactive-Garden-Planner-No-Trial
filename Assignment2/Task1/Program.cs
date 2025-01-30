ecbdc372b31a51db2f702421ac6406b4b80b1fcb
Iqbal Hassan Mahmud <iqbalmahmud.cs@gmail.com>
2025-01-30 13:02:34 +0600

    Task1/Program.cs

--- Assignment2/Task1/Program.cs
+++ Assignment2/Task1/Program.cs
@@ -1,9 +1,48 @@
-﻿Random random = new Random(DateTime.Now.Millisecond);
+﻿/*
+Task 1: 
+	=> Generating a 5GB Random Text File (33 Marks) 		
+	=> Generate a text file of exactly 5GB.	
+	=> Write random characters byte by byte (a method is already provided for this).	
+	=> Calculate the exact number of characters needed to reach 5GB.	
+	=> The generated file must be placed outside the project folder (alongside the solution file).	
+	=> Do not commit this file in the repository.	
+	=> If the file size is not exactly 5GB, you get zero marks.	
+*/
 
-for(int i = 0; i < 10; i++)
-    Console.WriteLine(GetRandomChar());
+using System.Text;
+using System.Xml;
 
-char GetRandomChar()
+string fileLocation = Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName, "RandomTextFile.txt");
+
+long maxFileSize = 5L * 1024 * 1024 * 1024;
+long currentFileSize = 0;
+
+using (FileStream fileStream = new FileStream(fileLocation, FileMode.Create, FileAccess.Write))
+using(StreamWriter fileWriter = new StreamWriter(fileStream, Encoding.UTF8))
+{
+	Random random = new Random(DateTime.Now.Millisecond);
+
+	while ( currentFileSize < maxFileSize)
+	{
+        char randomCharecter = GetRandomChar(random);
+        byte[] characterBytes = Encoding.UTF8.GetBytes(random.ToString());
+
+        fileWriter.BaseStream.Write(characterBytes, 0, characterBytes.Length);
+        currentFileSize += characterBytes.Length;
+
+        //  if (currentFileSize % (1024 * 1024 * 100) == 0)
+        //Console.WriteLine($"{currentFileSize / (1024 * 1024)} MB Written Succesfully.");
+    }
+
+}
+
+Console.WriteLine();
+
+
+//for (int i = 0; i < 10; i++)
+//    Console.WriteLine(GetRandomChar());
+
+static char GetRandomChar(Random random)
 {
     return (char)random.Next('A', 'Z');
 }
