fea9d40fd8f5772fe215c3eb062b1e4d26a7b365
Iqbal Hassan Mahmud <iqbalmahmud.cs@gmail.com>
2025-02-19 13:47:57 +0600

    worked on appsettings.json & DataUtility.cs

---
+++ final-project/src/AttendanceSystem/AttendanceSystem/DataUtility.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+
+namespace AttendanceSystem
+{
+    public static class DataUtility
+    {
+        public static string GetConnectionString()
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+
+            return config.GetConnectionString("DefaultConnection");
+        }
+    }
+}
--- final-project/src/AttendanceSystem/AttendanceSystem/Program.cs
+++ final-project/src/AttendanceSystem/AttendanceSystem/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using AttendanceSystem;
+
+var connectionString = DataUtility.GetConnectionString();
+Console.WriteLine($"Database Connection String: {connectionString}");
