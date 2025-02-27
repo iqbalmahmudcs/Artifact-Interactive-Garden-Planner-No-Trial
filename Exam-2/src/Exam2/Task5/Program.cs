1d711e2d2c86f768e7487e5e8623b0226b4ee2cc
Iqbal Hassan Mahmud <iqbalmahmud.cs@gmail.com>
2025-02-27 23:34:33 +0600

    Task5/Program.cs

--- Exam-2/src/Exam2/Task5/Program.cs
+++ Exam-2/src/Exam2/Task5/Program.cs
@@ -1,1 +1,5 @@
 ﻿(string Name, int Age) person = ("Jalaluddin", 20);
+
+var anonymusType = new { person.Name, person.Age };
+
+Console.WriteLine($"Name: {anonymusType.Name}, Age: {anonymusType.Age}");
